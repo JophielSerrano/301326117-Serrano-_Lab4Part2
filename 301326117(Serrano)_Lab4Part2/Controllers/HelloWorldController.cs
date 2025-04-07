695d5f24072469c9d84696dfb803f89b64b26e57
JophielSerrano <jserra27@my.centennialcollege.ca>
2025-04-06 20:14:58 -0400

    Add project files.

---
+++ 301326117(Serrano)_Lab4Part2/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace _301326117_Serrano__Lab4Part2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+
+}
---
+++ 301326117(Serrano)_Lab4Part2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _301326117_Serrano__Lab4Part2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
